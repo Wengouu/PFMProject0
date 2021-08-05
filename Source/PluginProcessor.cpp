7d3c63753b73a14b98242173a181ff3e4ae8bb6e
Wengou <gl@ohoh.eu>
2021-08-05 22:27:58 +0200

    Add a Noise Generator DSP code

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -144,17 +144,23 @@ void PFMProject0AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, 
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    juce::Random random;
 
-        // ..do something to the data...
+    for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
+    {
+        for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
+        {
+            if (shouldPlaySound)
+            {
+                buffer.setSample(channel, sample, random.nextFloat());
+            }
+            else 
+            {
+                buffer.setSample(channel, sample, 0);
+            }
+
+        }
+
     }
 }
 
